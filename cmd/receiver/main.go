468cda8c2c2ec866c00da8b55fc184e08c4085bf
Akash5106 <kvakash5106@gmail.com>
2026-07-05 16:29:35 +0530

    Implement UDP echo client/server

--- cmd/receiver/main.go
+++ cmd/receiver/main.go
@@ -1,7 +1,37 @@
-package receiver
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Receiver active")
+	addr, err := net.ResolveUDPAddr("udp", "localhost:5454")
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println("Error creating socket:", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println(addr)
+	buffer := make([]byte, 1024)
+	for {
+		n, addr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println("Received", n, "bytes of data from", addr)
+		fmt.Println(string(buffer[:n]))
+		_, err = conn.WriteToUDP(buffer[:n], addr)
+		if err != nil {
+			fmt.Println("Error sending mssg")
+			continue
+		}
+		fmt.Println("ACK sent")
+	}
 }
--- cmd/sender/main.go
+++ cmd/sender/main.go
@@ -1,7 +1,38 @@
-package sender
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Sender active")
+	addr, err := net.ResolveUDPAddr("udp", "localhost:5454")
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		fmt.Println("Error creating connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	message := []byte("Hello UDP")
+
+	_, err = conn.Write(message)
+	if err != nil {
+		fmt.Println("Error sending message:", err)
+		return
+	}
+
+	fmt.Println("Message sent!")
+	buffer := make([]byte, 1024)
+	n, addr, err := conn.ReadFromUDP(buffer)
+	if err != nil {
+		fmt.Println("Error receiving ACK")
+		return
+	}
+	fmt.Println("ACK received from ", addr, " : ", string(buffer[:n]))
 }
